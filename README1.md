139b2552e2d0c52a04d20da608c886eff03ca1b2
Mario Hernandez <mariohernandezk10@gmail.com>
2020-12-10 13:39:37 -0600

    finished it kinda

---
+++ README1.md
@@ -0,0 +1,29 @@
+# I don't have a title..
+
+## Description
+
+I am too lazy to write one..
+
+## Installation
+
+fjvbafjdnvjfkdv
+
+## Usage
+
+bfkjdsbvjkadsbnv
+
+## Credits
+
+I am the only collaborator
+
+## License
+
+No license needed
+
+## Badges
+
+I am not listing badges
+
+## Contributing
+
+just me, myself, and I
--- index.js
+++ index.js
@@ -1,29 +1,98 @@
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const fs = require('fs')
 
-var question = [
-    {
-        name: "userName",
+var question = [{
+        name: "title",
         type: "input",
-        message: "what is you name?",
-        default: "I don't have a name.."
+        message: "What is the title of your project?",
+        default: "I don't have a title.."
     },
     {
-        name: "age",
+        name: "description",
         type: "input",
-        message: "what is you age?",
-        default: "I lost count.."
+        message: "Write a description for your project",
+        default: "I am too lazy to write one.."
     },
     {
-        name: "born",
-        type: "list",
-        message: "Where were you born?",
-        choices: ["US", "France", "Mexico"],
-        default: "I been everywhere. I am everywhere. I am like the wind and gravity.."
+        name: "installation",
+        type: "input",
+        message: "What are the steps to install your project?",
+    },
+    {
+        name: "usage",
+        type: "input",
+        message: "Provide insctructions here",
+    },
+    {
+        name: "credits",
+        type: "input",
+        message: "List collaborators",
+        default: "I am the only collaborator",
+    },
+    {
+        name: "license",
+        choices: ["mit", "afl-3.0", "apache-2.0", "artistic-2.0", "bsl-1.0", "bsd-2-clause", "cc", "wtfpl"],
+        default: "No license needed",
+    },
+    {
+        name: "badges",
+        type: "input",
+        message: "List badges",
+        default: "I am not listing badges",
+    },
+    {
+        name: "contributor",
+        type: "input",
+        message: "Who else contributed?",
+        default: "just me, myself, and I",
     }
 ]
 
-inquirer.prompt(question).then(function(loggingAnswer){
-    console.log("Name: " + loggingAnswer.userName)
-    console.log("Age: " + loggingAnswer.age)
-    console.log("Born: " + loggingAnswer.born)
+// So you can use the name: _____ as a variable and save that in a object
+// hard code the titles and subtitle of the readeME
+
+inquirer.prompt(question).then(function (answers) {
+
+    const readMEFile = 
+`
+    ## Description
+    
+    ${answers.description}
+    
+    ## Installation
+    
+    ${answers.installation}
+    
+    ## Usage
+    
+    ${answers.usage}
+    
+    ## Credits
+    
+    ${answers.credits}
+    
+    ## License
+    
+    ${answers.license}
+    
+    ## Badges
+    
+    ${answers.badges}
+    
+    ## Contributing
+    
+    ${answers.contributor}`
+
+    fs.writeFile('README1.md', ``, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+    fs.appendFileSync('README1.md', `# ${answers.title}\n`, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+    fs.appendFileSync('README1.md', readMEFile, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
 });
